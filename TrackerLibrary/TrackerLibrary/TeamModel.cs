d51a530d02a8f4086e5936e8226df913a53dd415
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-06-02 10:50:56 +0200

    fix listname Person, PersonModel

--- TrackerLibrary/TrackerLibrary/TeamModel.cs
+++ TrackerLibrary/TrackerLibrary/TeamModel.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace TrackerLibrary
 {
-    class TeamModel
+    public class TeamModel
     {
+        // snippet: write prop and double tap. A property will hold one piece of data
+        // snippet2 we wont use for now: ctor: this is to create a constructor
+        // constructor: every time a class is instanciated it would call the constructor and instanciate the property in the constructor
+        // the reason we don't use it is because we can immediately instanciate it as you se below by adding = new List<Person>();
+        // auto property = new list of person
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
+
     }
 }
