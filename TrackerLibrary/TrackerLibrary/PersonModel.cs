29806341a1048581ca36f79e2a54af7e34a896e8
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-06-02 10:49:41 +0200

    add PersonModel

---
+++ TrackerLibrary/TrackerLibrary/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+   public class PersonModel
+    {
+        // move order for top effeciency:
+        // prop tab tab, write typehint, press enter, press tab, write name, enter, enter
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string CellphoneNumber { get; set; }
+    }
+}
