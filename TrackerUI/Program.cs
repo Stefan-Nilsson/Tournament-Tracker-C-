90d4da783411e9d6afc8fdc00aa26e623b3247ec
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-06-03 10:38:13 +0200

    add comments to program.cs

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -12,10 +12,14 @@ namespace TrackerUI
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+        // once the static void main method is done, the application closes.
         static void Main()
         {
+            // the first two lines say, just do what microsoft has set up
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            // dont end this line, untill this instance closes (tournamentdashboardform)
+            // this means that if you close the dasboardform the application will close.
             Application.Run(new TournamentDashboardForm());
         }
     }
