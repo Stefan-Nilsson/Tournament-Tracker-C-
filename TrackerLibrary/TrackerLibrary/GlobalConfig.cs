4b6ddb3a377e1e3a31bcfc4ac65f1b89e1b8bd4d
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-06-09 10:22:33 +0200

    set TODO - inside new Class

--- TrackerLibrary/TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/TrackerLibrary/GlobalConfig.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace TrackerLibrary
 {
-    class GlobalConfig
+    // Public static class == not a blueprint, it's the actual house and everyone uses the same house
+    // Normally we wouldn't put any data in here.
+    public static class GlobalConfig
+        // private set == only methods inside of this GlobalConfig class can change the value of Connections
+        // but everyone can read it
+        // We use a List so we can add one or more datasources to save to and pull from
     {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        // this method will be called on the startup of the application to say: these are the connections I want you to set up
+        // keep in mind potential updates or changes in the future
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO - Create the SQL Connection
+            }
+
+            if (textFiles)
+            {
+                // TODO - Create the Text Connection
+            }
+        }
     }
 }
