f9b7d689d10e169fc834f3ec2c89b78e976c19c1
Stef.Desk <contact.stefan.nilsson@gmail.com>
2020-06-10 12:35:26 +0200

    add Validation to CreatePrizeForm

--- TrackerUI/CreatePrizeForm.cs
+++ TrackerUI/CreatePrizeForm.cs
@@ -16,5 +16,39 @@ namespace TrackerUI
         {
             InitializeComponent();
         }
+
+        private void createPrizeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool ValidateForm()
+        {
+            bool output = true;
+            int placeNumber = 0;
+
+            // this uses an outparameter
+            // will return a bool value to placeNumberValidNumber, true if parse works, false if not
+            // At the same time it  will pass the parsed value to placeNumber
+            // try parse doesn't crash when it does not succeed, instead it returns false and 0
+            bool placeNumberValidNumber = int.TryParse(placeNumberValue.Text, out placeNumber);
+
+            if (placeNumberValidNumber == false)
+            {
+               output = false;
+            }
+
+            if (placeNumber < 1)
+            {
+                output = false;
+            }
+
+            if (placeNameValue.Text.Length == 0)
+            {
+                output = false;
+            }
+
+            return output;
+        }
     }
 }
